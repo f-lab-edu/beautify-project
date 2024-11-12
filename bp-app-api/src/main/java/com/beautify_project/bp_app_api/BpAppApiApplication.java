de833895a7580b3336535d7365b31cf3421c4936
sssukho <sssukho@gmail.com>
2024-11-12 12:21:24 +0900

    feat: 멀티 모듈 구조 구성

---
+++ bp-app-api/src/main/java/com/beautify_project/bp_app_api/BpAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.beautify_project.bp_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpAppApiApplication.class, args);
+	}
+
+}
---
+++ bp-utils/src/main/java/com/beautify_project/bp_utils/BpUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.beautify_project.bp_utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpUtilsApplication.class, args);
+	}
+
+}
