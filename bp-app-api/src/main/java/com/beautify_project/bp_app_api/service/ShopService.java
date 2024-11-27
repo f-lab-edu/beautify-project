cd8c7763eab1fffdac9a26e4811be649f5a6aef6
sssukho <sssukho@gmail.com>
2024-11-28 06:28:30 +0900

    refactor: 불필요 주석 삭제

--- bp-app-api/src/main/java/com/beautify_project/bp_app_api/service/ShopService.java
+++ bp-app-api/src/main/java/com/beautify_project/bp_app_api/service/ShopService.java
@@ -79,30 +79,4 @@ public class ShopService {
 
         return ResponseMessage.createResponseMessage(page.getContent());
     }
-
-    private List<ShopFindResult> createFindShopListDummySuccessResponseBody() {
-        final String[] shopIds = {"2360c169", "f4804d31"};
-
-        ShopFindResult result1 = ShopFindResult.builder()
-            .id(shopIds[0])
-            .name("시술소1")
-            .operations(Arrays.asList("두피문신", "눈썹문신", "입술문신"))
-            .supportFacilities(Arrays.asList("주차가능", "와이파이", "샤워실"))
-            .rate("4.5")
-            .likes(132)
-            .likePushed(false)
-            .build();
-
-        ShopFindResult result2 = ShopFindResult.builder()
-            .id(shopIds[1])
-            .name("시술소2")
-            .operations(List.of("타투"))
-            .supportFacilities(List.of("와이파이"))
-            .rate("3.0")
-            .likes(20)
-            .likePushed(true)
-            .build();
-
-        return Arrays.asList(result1, result2);
-    }
 }
