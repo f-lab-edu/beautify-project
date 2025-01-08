f23ad8450fb5be9bb7a0f3db5867aae759f8fdd4
sssukho <sssukho@gmail.com>
2025-01-09 03:28:35 +0900

    fix: 테스트 코드 수정 및 ShopLike bulkInsert 실패시 루프돌며 단일건 처리하도록 수정

--- bp-app-api/src/main/java/com/beautify_project/bp_app_api/service/ShopLikeService.java
+++ bp-app-api/src/main/java/com/beautify_project/bp_app_api/service/ShopLikeService.java
@@ -5,12 +5,16 @@ import com.beautify_project.bp_app_api.entity.ShopLike.ShopLikeId;
 import com.beautify_project.bp_app_api.repository.ShopLikeRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class ShopLikeService {
 
     private static final ShopLike EMPTY_SHOP_LIKE = ShopLike.of(null, null);
@@ -31,8 +35,23 @@ public class ShopLikeService {
     @Transactional
     public void saveAllShopLikes(final List<ShopLike> shopLikesToRegister) {
         // TODO: jdbc 사용하는 방식으로 변경?
-        shopLikeRepository.bulkInsert(shopLikesToRegister);
-//        shopLikeRepository.saveAll(shopLikesToRegister);
+        try {
+            shopLikeRepository.bulkInsert(shopLikesToRegister);
+        } catch (DataIntegrityViolationException dataIntegrityViolationException) {
+            saveShopLikeWithoutDuplicatedKey(shopLikesToRegister);
+        }
+    }
+
+    @Transactional
+    private void saveShopLikeWithoutDuplicatedKey(final List<ShopLike> shopLikesToRegister) {
+        // TODO: org.springframework.transaction.UnexpectedRollbackException: Transaction silently rolled back because it has been marked as rollback-only 해결 필요
+        for (ShopLike shopLike : shopLikesToRegister) {
+            try {
+                shopLikeRepository.save(shopLike);
+            } catch (DuplicateKeyException duplicateKeyException) {
+                log.error("Failed to insert ShopLike {}", shopLike, duplicateKeyException);
+            }
+        }
     }
 
     @Transactional
--- bp-app-api/src/test/java/com/beautify_project/bp_app_api/integration/ShopServiceRepositoryIntegrationTest.java
+++ bp-app-api/src/test/java/com/beautify_project/bp_app_api/integration/ShopServiceRepositoryIntegrationTest.java
@@ -2,8 +2,12 @@ package com.beautify_project.bp_app_api.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.beautify_project.bp_app_api.dto.common.ErrorResponseMessage.ErrorCode;
+import com.beautify_project.bp_app_api.dto.event.ShopLikeEvent;
 import com.beautify_project.bp_app_api.dto.shop.ShopRegistrationRequest;
 import com.beautify_project.bp_app_api.dto.shop.ShopRegistrationRequest.Address;
 import com.beautify_project.bp_app_api.dto.shop.ShopRegistrationRequest.BusinessTime;
@@ -94,282 +98,229 @@ class ShopServiceRepositoryIntegrationTest {
     }
 
     @Test
-    @DisplayName("Shop 좋아요시 db에 기존 샵의 좋아요 수 + 1 과 ShopLike db에 저장되어야 한다.")
-    void given_shopLike_when_succeed_then_db_inserted() {
+    @DisplayName("Shop 좋아요 이벤트 배치 처리 성공시 모든 샵의 좋아요 수가 이벤트 수 만큼 성공한다.")
+    void given_batchShopLikes_when_succeed_then_db_updated() {
         // given
-        final List<Operation> mockedOperationEntities = Arrays.asList(
+        final List<Operation> operationEntitiesForPreData = Arrays.asList(
             Operation.of("두피 문신 시술", "두피 문신 시술 설명"),
-            Operation.of("점 제거 시술", "점 제거 시술 설명"));
+            Operation.of("점 제거 시술", "점 제거 시술 설명")
+        );
 
-        final List<Facility> mockedFacilityEntities = Arrays.asList(
+        final List<Facility> facilityEntitiesForPreData = Arrays.asList(
             Facility.withName("와이파이"),
             Facility.withName("샤워실")
         );
 
-        final List<String> mockedOperationIds = Arrays.asList(
-            mockedOperationEntities.get(0).getId(), mockedOperationEntities.get(1).getId());
-        final List<String> mockedFacilityIds = Arrays.asList(
-            mockedFacilityEntities.get(0).getId(), mockedFacilityEntities.get(1).getId()
+        final List<String> operationIdsForPreData = Arrays.asList(
+            operationEntitiesForPreData.get(0).getId(), operationEntitiesForPreData.get(1).getId()
         );
 
-        final ShopRegistrationRequest requestForTest = new ShopRegistrationRequest(
-            "미용시술소1",
-            "010-1234-5678",
-            "www.naer.com",
-            "안녕하세요 미용시술소1입니다.",
-            mockedOperationIds,
-            mockedFacilityIds,
-            Arrays.asList("preSigned-url1", "preSigned-url2"),
-            new BusinessTime(
-                LocalTime.of(9, 0),
-                LocalTime.of(18, 0),
-                LocalTime.of(13, 0),
-                LocalTime.of(14, 0),
-                Arrays.asList("monday", "tuesday")),
-            new Address(
-                "111",
-                "서울시",
-                "마포구",
-                "상암동",
-                "481",
-                "월드컵북로",
-                "true",
-                "131",
-                "707",
-                "오벨리스크",
-                "134-070",
-                "주상복합",
-                "12345678",
-                "34",
-                "90"
-            )
+        final List<String> facilityIdsForPreData = Arrays.asList(
+            facilityEntitiesForPreData.get(0).getId(), facilityEntitiesForPreData.get(1).getId()
         );
 
-        final Shop testShop = Shop.from(requestForTest);
-        shopRepository.saveAndFlush(testShop); // DB 에 있는 상태 가정
+        final List<ShopRegistrationRequest> shopRegistrationRequestsForPreData = Arrays.asList(
+            new ShopRegistrationRequest("미용시술소1",
+                "010-1234-5678",
+                "www.naer.com",
+                "안녕하세요 미용시술소1입니다.",
+                operationIdsForPreData,
+                facilityIdsForPreData,
+                Arrays.asList("preSigned-url1", "preSigned-url2"),
+                new BusinessTime(
+                    LocalTime.of(9, 0),
+                    LocalTime.of(18, 0),
+                    LocalTime.of(13, 0),
+                    LocalTime.of(14, 0),
+                    Arrays.asList("monday", "tuesday")),
+                new Address(
+                    "111",
+                    "서울시",
+                    "마포구",
+                    "상암동",
+                    "481",
+                    "월드컵북로",
+                    "true",
+                    "131",
+                    "707",
+                    "오벨리스크",
+                    "134-070",
+                    "주상복합",
+                    "12345678",
+                    "34",
+                    "90")
+            ),
+            new ShopRegistrationRequest("미용시술소2",
+                "010-1234-5678",
+                "www.naer.com",
+                "안녕하세요 미용시술소1입니다.",
+                operationIdsForPreData,
+                facilityIdsForPreData,
+                Arrays.asList("preSigned-url1", "preSigned-url2"),
+                new BusinessTime(
+                    LocalTime.of(9, 0),
+                    LocalTime.of(18, 0),
+                    LocalTime.of(13, 0),
+                    LocalTime.of(14, 0),
+                    Arrays.asList("monday", "tuesday")),
+                new Address(
+                    "111",
+                    "서울시",
+                    "마포구",
+                    "상암동",
+                    "481",
+                    "월드컵북로",
+                    "true",
+                    "131",
+                    "707",
+                    "오벨리스크",
+                    "134-070",
+                    "주상복합",
+                    "12345678",
+                    "34",
+                    "90")
+            ));
 
-        final String memberEmail = "sssukho@gmail.com";
+        final List<Shop> shopsForPreData = shopRegistrationRequestsForPreData.stream()
+            .map(Shop::from).toList();
+        shopRepository.saveAllAndFlush(shopsForPreData);
+
+        final List<String> memberEmailsForFirstShop = Arrays.asList(
+            "dev1.sssukho@gmail.com", "dev2.sssukho@gmail.com", "dev3.sssukho@gmail.com");
+
+        final String memberEmailForSecondShop = "dev4.sssukho@gmail.com";
+
+        final List<ShopLikeEvent> shopLikeEvents = Arrays.asList(
+            new ShopLikeEvent(shopsForPreData.get(0).getId(), memberEmailsForFirstShop.get(0)),
+            new ShopLikeEvent(shopsForPreData.get(0).getId(), memberEmailsForFirstShop.get(1)),
+            new ShopLikeEvent(shopsForPreData.get(0).getId(), memberEmailsForFirstShop.get(2)),
+            new ShopLikeEvent(shopsForPreData.get(1).getId(), memberEmailForSecondShop)
+        );
 
         // when
-        shopService.likeShop(testShop.getId(), memberEmail);
+        shopService.batchShopLikes(shopLikeEvents);
 
         // then
-        final Shop shopFromDb = shopRepository.findById(testShop.getId())
-            .orElseThrow(() -> new NotFoundException(
-                ErrorCode.SH001));
-
-        final Long previousLikeCount = testShop.getLikes();
-        final Long postLikeCount = shopFromDb.getLikes();
-
-        final ShopLike shopLikeFromDb = shopLikeRepository.findAll().get(0);
-
-        shopLikeRepository.findById(ShopLikeId.of(shopFromDb.getId(), memberEmail));
-
-        assertThat(postLikeCount).isEqualTo(previousLikeCount + 1);
-        assertThat(shopLikeFromDb.getId().getShopId()).isEqualTo(shopFromDb.getId());
-        assertThat(shopLikeFromDb.getId().getMemberEmail()).isEqualTo(memberEmail);
+        assertThat(
+            shopRepository.findById(shopsForPreData.get(0).getId()).get().getLikes()).isEqualTo(3);
+        assertThat(
+            shopRepository.findById(shopsForPreData.get(1).getId()).get().getLikes()).isEqualTo(1);
+        assertThat(shopLikeRepository.count()).isEqualTo(4);
     }
 
     @Test
-    @DisplayName("Shop 좋아요 취소시 DB에 기존 샵의 좋아요 수 - 1 과 ShopLike db에 저장되어 있지 않아야 한다.")
-    void given_shopLikeCancel_when_succeed_then_db_deleted() {
+    @DisplayName("Shop 좋아요 이벤트 배치 처리 실패시 모든 샵의 좋아요 수는 반영되지 않는다.")
+    void given_batchShopLikes_when_failed_then_db_rollback() {
         // given
-        final List<Operation> mockedOperationEntities = Arrays.asList(
+        final List<Operation> operationEntitiesForPreData = Arrays.asList(
             Operation.of("두피 문신 시술", "두피 문신 시술 설명"),
-            Operation.of("점 제거 시술", "점 제거 시술 설명"));
+            Operation.of("점 제거 시술", "점 제거 시술 설명")
+        );
 
-        final List<Facility> mockedFacilityEntities = Arrays.asList(
+        final List<Facility> facilityEntitiesForPreData = Arrays.asList(
             Facility.withName("와이파이"),
             Facility.withName("샤워실")
         );
 
-        final List<String> mockedOperationIds = Arrays.asList(
-            mockedOperationEntities.get(0).getId(), mockedOperationEntities.get(1).getId());
-        final List<String> mockedFacilityIds = Arrays.asList(
-            mockedFacilityEntities.get(0).getId(), mockedFacilityEntities.get(1).getId()
+        final List<String> operationIdsForPreData = Arrays.asList(
+            operationEntitiesForPreData.get(0).getId(), operationEntitiesForPreData.get(1).getId()
         );
 
-        final ShopRegistrationRequest requestForTest = new ShopRegistrationRequest(
-            "미용시술소1",
-            "010-1234-5678",
-            "www.naer.com",
-            "안녕하세요 미용시술소1입니다.",
-            mockedOperationIds,
-            mockedFacilityIds,
-            Arrays.asList("preSigned-url1", "preSigned-url2"),
-            new BusinessTime(
-                LocalTime.of(9, 0),
-                LocalTime.of(18, 0),
-                LocalTime.of(13, 0),
-                LocalTime.of(14, 0),
-                Arrays.asList("monday", "tuesday")),
-            new Address(
-                "111",
-                "서울시",
-                "마포구",
-                "상암동",
-                "481",
-                "월드컵북로",
-                "true",
-                "131",
-                "707",
-                "오벨리스크",
-                "134-070",
-                "주상복합",
-                "12345678",
-                "34",
-                "90"
-            )
+        final List<String> facilityIdsForPreData = Arrays.asList(
+            facilityEntitiesForPreData.get(0).getId(), facilityEntitiesForPreData.get(1).getId()
         );
 
-        final Shop testShop = Shop.from(requestForTest);
-        testShop.increaseLikeCount();
-        shopRepository.saveAndFlush(testShop);
+        final List<ShopRegistrationRequest> shopRegistrationRequestsForPreData = Arrays.asList(
+            new ShopRegistrationRequest("미용시술소1",
+                "010-1234-5678",
+                "www.naer.com",
+                "안녕하세요 미용시술소1입니다.",
+                operationIdsForPreData,
+                facilityIdsForPreData,
+                Arrays.asList("preSigned-url1", "preSigned-url2"),
+                new BusinessTime(
+                    LocalTime.of(9, 0),
+                    LocalTime.of(18, 0),
+                    LocalTime.of(13, 0),
+                    LocalTime.of(14, 0),
+                    Arrays.asList("monday", "tuesday")),
+                new Address(
+                    "111",
+                    "서울시",
+                    "마포구",
+                    "상암동",
+                    "481",
+                    "월드컵북로",
+                    "true",
+                    "131",
+                    "707",
+                    "오벨리스크",
+                    "134-070",
+                    "주상복합",
+                    "12345678",
+                    "34",
+                    "90")
+            ),
+            new ShopRegistrationRequest("미용시술소2",
+                "010-1234-5678",
+                "www.naer.com",
+                "안녕하세요 미용시술소1입니다.",
+                operationIdsForPreData,
+                facilityIdsForPreData,
+                Arrays.asList("preSigned-url1", "preSigned-url2"),
+                new BusinessTime(
+                    LocalTime.of(9, 0),
+                    LocalTime.of(18, 0),
+                    LocalTime.of(13, 0),
+                    LocalTime.of(14, 0),
+                    Arrays.asList("monday", "tuesday")),
+                new Address(
+                    "111",
+                    "서울시",
+                    "마포구",
+                    "상암동",
+                    "481",
+                    "월드컵북로",
+                    "true",
+                    "131",
+                    "707",
+                    "오벨리스크",
+                    "134-070",
+                    "주상복합",
+                    "12345678",
+                    "34",
+                    "90")
+            ));
 
-        final String memberEmail = "sssukho@gmail.com";
+        final List<Shop> shopsForPreData = shopRegistrationRequestsForPreData.stream()
+            .map(Shop::from).toList();
+        shopRepository.saveAllAndFlush(shopsForPreData);
 
-        ShopLike shopLike = ShopLike.of(testShop.getId(), memberEmail);
-        shopLikeRepository.saveAndFlush(shopLike);
+        final List<String> memberEmailsForFirstShop = Arrays.asList(
+            "dev1.sssukho@gmail.com", "dev2.sssukho@gmail.com", "dev3.sssukho@gmail.com");
+
+        final String memberEmailForSecondShop = "dev4.sssukho@gmail.com";
+
+        shopLikeRepository.save(
+            ShopLike.of(shopsForPreData.get(0).getId(), memberEmailsForFirstShop.get(0)));
+
+        final List<ShopLikeEvent> shopLikeEvents = Arrays.asList(
+            new ShopLikeEvent(shopsForPreData.get(0).getId(), memberEmailsForFirstShop.get(0)),
+            new ShopLikeEvent(shopsForPreData.get(0).getId(), memberEmailsForFirstShop.get(1)),
+            new ShopLikeEvent(shopsForPreData.get(0).getId(), memberEmailsForFirstShop.get(2)),
+            new ShopLikeEvent(shopsForPreData.get(0).getId(), memberEmailsForFirstShop.get(0)),
+            new ShopLikeEvent(shopsForPreData.get(1).getId(), memberEmailForSecondShop)
+        );
 
         // when
-        shopService.cancelLikeShop(testShop.getId(), memberEmail);
+        shopService.batchShopLikes(shopLikeEvents);
 
         // then
-        assert testShop.getId() != null;
-        final Shop shopFromDB = shopRepository.findById(testShop.getId())
-            .orElseThrow(() -> new NotFoundException(ErrorCode.SH001));
-
-        final Long previousLikeCount = testShop.getLikes();
-        final Long postLikeCount = shopFromDB.getLikes();
-
-        assertThat(shopLikeRepository.count()).isZero();
-        assertThat(postLikeCount).isEqualTo(previousLikeCount - 1);
-    }
-
-    @Test
-    @DisplayName("Shop 좋아요시 기존에 좋아요 한 이력이 있으면 AlreadyProcessedException 을 던진다")
-    void given_shopLike_when_already_shop_like_exists_then_throw_alreadyProcessedException() {
-        // given
-        final List<Operation> mockedOperationEntities = Arrays.asList(
-            Operation.of("두피 문신 시술", "두피 문신 시술 설명"),
-            Operation.of("점 제거 시술", "점 제거 시술 설명"));
-
-        final List<Facility> mockedFacilityEntities = Arrays.asList(
-            Facility.withName("와이파이"),
-            Facility.withName("샤워실")
-        );
-
-        final List<String> mockedOperationIds = Arrays.asList(
-            mockedOperationEntities.get(0).getId(), mockedOperationEntities.get(1).getId());
-        final List<String> mockedFacilityIds = Arrays.asList(
-            mockedFacilityEntities.get(0).getId(), mockedFacilityEntities.get(1).getId()
-        );
-
-        final ShopRegistrationRequest requestForTest = new ShopRegistrationRequest(
-            "미용시술소1",
-            "010-1234-5678",
-            "www.naer.com",
-            "안녕하세요 미용시술소1입니다.",
-            mockedOperationIds,
-            mockedFacilityIds,
-            Arrays.asList("preSigned-url1", "preSigned-url2"),
-            new BusinessTime(
-                LocalTime.of(9, 0),
-                LocalTime.of(18, 0),
-                LocalTime.of(13, 0),
-                LocalTime.of(14, 0),
-                Arrays.asList("monday", "tuesday")),
-            new Address(
-                "111",
-                "서울시",
-                "마포구",
-                "상암동",
-                "481",
-                "월드컵북로",
-                "true",
-                "131",
-                "707",
-                "오벨리스크",
-                "134-070",
-                "주상복합",
-                "12345678",
-                "34",
-                "90"
-            )
-        );
-
-        final Shop testShop = Shop.from(requestForTest);
-        final String memberEmail = "sssukho@gmail.com";
-        shopRepository.saveAndFlush(testShop);
-        shopService.likeShop(testShop.getId(), memberEmail);
-
-        // when & then
-        assertThatThrownBy(() -> shopService.likeShop(testShop.getId(), memberEmail)).isInstanceOf(
-            AlreadyProcessedException.class);
-    }
-
-    @Test
-    @DisplayName("Shop 좋아요 취소시 기존에 좋아요 했던 이력이 없으면 AlreadyProcessedException 을 던진다")
-    void given_shopLikeCancel_when_shop_like_not_exists_then_throw_alreadyProcessedException() {
-        // given
-        final List<Operation> mockedOperationEntities = Arrays.asList(
-            Operation.of("두피 문신 시술", "두피 문신 시술 설명"),
-            Operation.of("점 제거 시술", "점 제거 시술 설명"));
-
-        final List<Facility> mockedFacilityEntities = Arrays.asList(
-            Facility.withName("와이파이"),
-            Facility.withName("샤워실")
-        );
-
-        final List<String> mockedOperationIds = Arrays.asList(
-            mockedOperationEntities.get(0).getId(), mockedOperationEntities.get(1).getId());
-        final List<String> mockedFacilityIds = Arrays.asList(
-            mockedFacilityEntities.get(0).getId(), mockedFacilityEntities.get(1).getId()
-        );
-
-        final ShopRegistrationRequest requestForTest = new ShopRegistrationRequest(
-            "미용시술소1",
-            "010-1234-5678",
-            "www.naer.com",
-            "안녕하세요 미용시술소1입니다.",
-            mockedOperationIds,
-            mockedFacilityIds,
-            Arrays.asList("preSigned-url1", "preSigned-url2"),
-            new BusinessTime(
-                LocalTime.of(9, 0),
-                LocalTime.of(18, 0),
-                LocalTime.of(13, 0),
-                LocalTime.of(14, 0),
-                Arrays.asList("monday", "tuesday")),
-            new Address(
-                "111",
-                "서울시",
-                "마포구",
-                "상암동",
-                "481",
-                "월드컵북로",
-                "true",
-                "131",
-                "707",
-                "오벨리스크",
-                "134-070",
-                "주상복합",
-                "12345678",
-                "34",
-                "90"
-            )
-        );
-
-        final Shop testShop = Shop.from(requestForTest);
-        testShop.increaseLikeCount();
-        shopRepository.saveAndFlush(testShop);
-
-        final String memberEmail = "sssukho@gmail.com";
-
-        // when & then
-        assertThatThrownBy(
-            () -> shopService.cancelLikeShop(testShop.getId(), memberEmail)).isInstanceOf(
-            AlreadyProcessedException.class);
+        assertThat(
+            shopRepository.findById(shopsForPreData.get(0).getId()).get().getLikes()).isEqualTo(0);
+        assertThat(
+            shopRepository.findById(shopsForPreData.get(1).getId()).get().getLikes()).isEqualTo(0);
+        assertThat(shopLikeRepository.count()).isEqualTo(0);
     }
 
     @Test
